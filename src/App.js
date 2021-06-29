ab1fddefd6bfc9e3b6e54d9c6aa087f9bbd94ae3
You Song Hou <yousonghou@yahoo.com>
2021-06-29 17:52:04 -0400

    set up bitgo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
 
+import './App.css';
+import { BITGO_APIKEY } from './config';
 import Main from './components/Main';
 
 function App() {
+  useEffect(() => {
+    const initializeBitgo = async () => {
+      const BitGo = require('bitgo');
+      const bitgo = new BitGo.BitGo({ accessToken: BITGO_APIKEY }); // defaults to testnet. add env: 'prod' if you want to go against mainnet
+      // const result = await bitgo.session();
+      console.log(bitgo);
+    }
+
+    initializeBitgo();
+  }, [])
   return (
     <div className="App">
       <Main />
